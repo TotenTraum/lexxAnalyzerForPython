332062eac8e3471cb177fdd6bad717209537b994
TotenTraum <81190163+TotenTraum@users.noreply.github.com>
2022-12-09 20:39:08 +0400

    miniCommit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ mtx = [[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (6, 1), 
        [(1, 0), (8, 1), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0), (1, 0)],  # 1 is string
        [(0, 1), (1, 1), (2, 0), (3, 1), (4, 1), (9, 1), (9, 1), (7, 1), (8, 1), (9, 1)],  # 2 is number
        [(0, 1), (1, 1), (3, 0), (3, 0), (4, 1), (9, 1), (9, 1), (7, 1), (8, 1), (9, 1)],  # 3 is name
-       [(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (9, 1), (9, 1), (7, 1), (8, 1), (9, 1)],  # 4 is op
+       [(0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (9, 1), (9, 1), (7, 1), (8, 1), (9, 0)],  # 4 is op
        [(1, 0), (1, 1), (2, 1), (3, 1), (4, 1), (5, 0), (9, 1), (7, 0), (5, 0), (9, 1)],  # 5 is indent  !5 6 7
        [(1, 0), (1, 1), (2, 1), (3, 1), (4, 1), (5, 0), (6, 0), (7, 1), (6, 0), (9, 1)],  # 6 is dedent  !5 6 7
        [(0, 1), (7, 0), (7, 0), (7, 0), (7, 0), (9, 1), (9, 1), (7, 0), (7, 0), (7, 0)],  # 7 is comment
@@ -51,8 +51,8 @@ def type_of(ch):
     match_number = re.fullmatch(r'\d', ch)
     match_char = re.fullmatch(r'\w', ch)
     match_whitespace = re.fullmatch(r' ', ch)
-    match_operator = re.fullmatch(r'[\[\]{}()+\-*/=&%|:,]', ch)
-    match_string = re.fullmatch(r'\'', ch)
+    match_operator = re.fullmatch(r'[\[\]{}()+\-\\*/=&%|:,.;><!_]', ch)
+    match_string = re.fullmatch(r'[\'\"]', ch)
     match_nl = re.fullmatch(r'\n', ch)
     match_comment = re.fullmatch(r'#', ch)
     if match_number:
@@ -80,7 +80,19 @@ def type_to_state(cur_state, symbol_type):
         return equ_id[symbol_type]
 
 
-def next_state(cur_state, ch, level_stack: deque, dedent):
+def is_double_op(first_char, second_char):
+    if second_char == '=':
+        return first_char == '=' or first_char == '<' or first_char == '>' or first_char == '!'
+    if second_char == '*' and first_char == '*':
+        return second_char == '*' and first_char == '*'
+    if second_char == '\\' and first_char == '\\':
+        return second_char == '\\' and first_char == '\\'
+    return 0 == 1
+
+
+def next_state(cur_state, str, i, level_stack: deque, op_stack: deque,
+               prev_str_symbol: deque, str_symbol_stack: deque, dedent):
+    ch = str[i]
     symbol_type = type_of(ch)
     state = type_to_state(cur_state, symbol_type)
     if state == equ_id['newline']:
@@ -95,9 +107,57 @@ def next_state(cur_state, ch, level_stack: deque, dedent):
                 state = equ_id['indent']
             level_stack[0] = cur_level
 
+    if cur_state == equ_id['string'] and state == equ_id['string']:
+        if len(op_stack) > 0:
+            op = op_stack.pop()
+            if op == '\\':
+                state = equ_id['name']
+            else:
+                op_stack.clear()
+
+
+    if state == equ_id['string']:
+        if cur_state == equ_id['string']:
+            if len(prev_str_symbol) > 0:
+                if prev_str_symbol[0] != ch:
+                    state = equ_id['name']
+                else:
+                    if len(str_symbol_stack) == 1 and str_symbol_stack[0] == ch:
+                        if len(str) >= i + 1 and ch == str[i+1]:
+                            state = equ_id['name']
+
+                    str_symbol_stack.append(ch)
+
+        else:
+            prev_str_symbol.clear()
+            prev_str_symbol.append(ch)
+            str_symbol_stack.append(ch)
+    else:
+        str_symbol_stack.clear()
+
+
+    # double op check
+    if cur_state == equ_id['op'] and state == equ_id['op']:
+        if len(op_stack) == 1:
+            if is_double_op(op_stack[0], ch):
+                if len(str) >= i + 1:
+                    nxt_state = type_to_state(state, type_of(str[i + 1]))
+                    i += 1
+                    state = nxt_state
+                    op_stack.clear()
+        else:
+            state = equ_id['nope']
+        op_stack.clear()
+
+    # push to op_stack if state == op, else clear op_stack
+    if state == equ_id['op']:
+        op_stack.append(ch)
+    else:
+        op_stack.clear()
+
     if cur_state == equ_id['newline'] and state != equ_id['dedent'] and level_stack[1] > 0:
         dedent = 1
-    return state, dedent
+    return i, state, dedent
 
 
 def format_string(result: list, cur_state, index, last_pos, string: str, dedent):
@@ -107,25 +167,30 @@ def format_string(result: list, cur_state, index, last_pos, string: str, dedent)
     if not (cur_state == equ_id['indent'] or cur_state == equ_id['dedent']):
         print_string = print_string.strip()
     if cur_state == equ_id['newline']:
-        result.append("%10s  |  %10s" % (equ_name[cur_state], 'newline'))
+        result.append("(%5i, %5i) %10s  |  %10s" % (last_pos, index, equ_name[cur_state], 'newline'))
         if dedent == 1:
-            result.append("%10s  |  %10s" % ('dedent', "''"))
+            result.append("(%5i, %5i) %10s  |  %10s" % (last_pos, index, 'dedent', "''"))
             dedent = 0
     else:
-        result.append("%10s  |  %10s" % (equ_name[cur_state], "'"+print_string+"'"))
+        result.append("(%5i, %5i) %10s  |  %10s" % (last_pos, index, equ_name[cur_state], "'"+print_string+"'"))
     last_pos = index
     return index, last_pos, dedent
 
 
 def analyze(string: str):
     string += ' '
+    prev_str_symbols = deque([])
+    str_symbols_stack = deque([])
     result = []
     level_stack = deque([0, 0])
+    sym_stack = deque([])
     last_pos = 0
     dedent = 0
     cur_state = 8  # start from divider
+    print(len(string))
     for i in range(0, len(string)):
-        nxt_state, dedent = next_state(cur_state, string[i], level_stack, dedent)
+        i, nxt_state, dedent = next_state(cur_state, string, i, level_stack, sym_stack,
+                                          prev_str_symbols , str_symbols_stack, dedent)
         (to_state, end_state) = mtx[cur_state][nxt_state]
         if end_state == 1:
             i, last_pos, dedent = format_string(result, cur_state, i, last_pos, string, dedent)
@@ -133,21 +198,11 @@ def analyze(string: str):
     return result
 
 
-prog_example = """
-for 2i in range(100):  # comment
-if i % 1 == 0:
-    print(':', i ** 2 ++ '212')
-print(':')
- """
+prog_example = io.open("main.py").read()
 
 
 def format_input_lang(string: str):
     return string.strip().replace('\t', '    ')
-
-
-rl = io.StringIO(prog_example).readline
-for t_type, t_str, (br, bc), (er, ec), logl in tokenize.generate_tokens(rl):
-    print("%3i %10s : %20r" % (t_type, token.tok_name[t_type], t_str))
 
 var = analyze(format_input_lang(prog_example))
 for a in var:
